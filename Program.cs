b3bfb6b6f14c2272bb7ac296e495606e377879f8
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2023-11-03 15:41:25 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,149 +1,22 @@
-﻿using DatenbankReader.Logic;
-using DatenbankReader.Model;
-using Npgsql;
-using System.Data.Common;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-class Program
+namespace InvoiceManagement
 {
-
-    static void Main(string[] args)
+    internal static class Program
     {
-
-        /*
-        if (args.Length != 1)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Console.WriteLine("Info - DatenbankReader");
-            Console.WriteLine("-artikel   Auslesen der Artikel");
-        }
-        else
-        {
-            CSVReadWrite csv = new CSVReadWrite();
-            DBReadWrite db = new DBReadWrite();
-
-            switch (args[0])
-            {
-                case "-kunde":
-                    ProcessKunde(csv, db);
-                    break;
-                case "-artikel":
-                    ProcessArtikel(csv, db);
-                    break;
-                case "-rechnung":
-                    ProcessRechnung(csv, db);
-                    break;
-                case "-all":
-                    ProcessKunde(csv, db);
-                    ProcessArtikel(csv, db);
-                    ProcessRechnung(csv, db);
-                    break;
-                default:
-                    Console.WriteLine("Ungültige Option: " + args[0]);
-                    break;
-            }
-        }*/
-
-        // FUER GESAMTBETRAG
-        string connString = "Server=192.168.1.236;Port=5432;Database=ausbildung;User Id=nicetec;Password=nicetec;";
-
-        if (args.Length != 1)
-        {
-            Console.WriteLine("Info - DatenbankReader");
-            Console.WriteLine("-artikel   Auslesen der Artikel");
-        }
-        else
-        {
-            CSVReadWrite csv = new CSVReadWrite();
-            DBReadWrite db = new DBReadWrite();
-
-            if (args[0] == "-kunde")
-            {
-                ProcessKunde(csv, db);               
-            }
-            else if (args[0] == "-artikel")
-            {
-                ProcessArtikel(csv, db);
-            }
-            else if (args[0] == "-rechnung")
-            {
-                ProcessRechnung(csv, db);
-            }
-            else if(args[0] == "-all")
-            {
-                ProcessKunde(csv, db);
-                ProcessArtikel(csv, db);
-                ProcessRechnung(csv, db);
-
-                using (NpgsqlConnection conn = new NpgsqlConnection(connString))
-                {
-                    conn.Open();
-                    GesamtbetragProKunde(conn);
-                    conn.Close();
-                }
-            }
-        }
-    }
-
-    public static void ProcessKunde(CSVReadWrite csv, DBReadWrite db)
-    {
-        List<Kunde> kunden = csv.KundeFromCSV();
-
-        foreach (var kunde in kunden)
-        {
-            db.WriteKundeToDatabase(kunde);
-        }
-    }
-
-    public static void ProcessArtikel(CSVReadWrite csv, DBReadWrite db)
-    {
-        List<Artikel> artikel = csv.ArtikelFromCSV();
-
-        foreach (var a in artikel)
-        {
-            db.WriteArtikelToDatabase(a);
-        }
-    }
-
-    public static void ProcessRechnung(CSVReadWrite csv, DBReadWrite db)
-    {
-        List<Rechnung> rechnungen = csv.RechnungFromCSV();
-
-        foreach (var rechnung in rechnungen)
-        {
-            db.WriteRechnungToDatabase(rechnung);
-        }
-    }
-
-    
-    public static void GesamtbetragProKunde(NpgsqlConnection _conn)
-    {
-        using (var cmd = new NpgsqlCommand("SELECT k.kundenid, k.name AS kundenname, SUM(r.anzahl * a.preis) AS gesamtbetrag " +
-                                          "FROM kunde k " +
-                                          "JOIN rechnung r ON k.kundenid = r.kundenid " +
-                                          "JOIN artikel a ON r.artikelid = a.artikelid " +
-                                          "GROUP BY k.kundenid, k.name", _conn))
-
-        using (var reader = cmd.ExecuteReader())
-        {
-            Console.WriteLine("---------------Gesamtbetrag--------------");
-            Console.WriteLine("Gesamtbetrag pro Kunde:");
-            while (reader.Read())
-            {
-                int kundenId = reader.GetInt32(0);
-                string kundenname = reader.GetString(1);
-
-                // Check if gesamtbetrag is null
-                if (!reader.IsDBNull(2))
-                {
-                    double gesamtbetrag = reader.GetDouble(2);
-                    Console.WriteLine($"Kunde: {kundenname}, Gesamtbetrag: {gesamtbetrag:C}");
-                }
-                else
-                {
-                    Console.WriteLine($"Kunde: {kundenname}, Gesamtbetrag: N/A (NULL)");
-                }
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new formMain());
         }
     }
 }
---
+++ formMain.cs
@@ -0,0 +1,265 @@
+﻿using MySqlConnector;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvoiceManagement
+{
+    public partial class formMain : Form
+    {
+        private DataTable dbDataTable = new DataTable();
+
+        public formMain()
+        {
+            InitializeComponent();
+        }
+        //MEINE CONN DATEN!!!
+        //_connection = new NpgsqlConnection("Server=192.168.1.236;Port=5432;Database=ausbildung;User Id=nicetec;Password=nicetec;");
+
+        private void RetriveDataKunden_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = GetConnectionString();
+
+                using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    string queryKunde = "SELECT * FROM kunde;";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(queryKunde, con))
+                    {
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            DataTable table = new DataTable();
+                            table.Load(reader);
+                            dataGridViewKunde.DataSource = table;
+                            //con.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler beim Verbinden zur Datenbank: " + ex.Message);
+            }
+        }
+
+        private void RetriveDataArtikel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = GetConnectionString();
+
+                using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    string queryKunde = "SELECT * FROM artikel;";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(queryKunde, con))
+                    {
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            DataTable table = new DataTable();
+                            table.Load(reader);
+                            dataGridViewArtikel.DataSource = table;
+                            //con.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler beim Verbinden zur Datenbank: " + ex.Message);
+            }
+        }
+
+        private void RetriveDataRechnung_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = GetConnectionString();
+
+                using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    string queryKunde = "SELECT * FROM rechnung;";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(queryKunde, con))
+                    {
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            DataTable table = new DataTable();
+                            table.Load(reader);
+                            dataGridViewRechnung.DataSource = table;
+                            //con.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler beim Verbinden zur Datenbank: " + ex.Message);
+            }
+        }
+
+        private string GetConnectionString()
+        {
+            string server = "192.168.1.236";
+            string port = "5432";
+            string database = "ausbildung";
+            string user = "nicetec";
+            string passwort = "nicetec";
+
+            string connectionString = $"Host={server};Port={port};Database={database};Username={user};Password={passwort}";
+
+            return connectionString;
+        }
+
+        private void dataGridViewArtikeln_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) 
+            {
+                string searchString = textBox1.Text.Trim(); 
+
+                if (!string.IsNullOrEmpty(searchString))
+                {
+                    
+                    string query = "SELECT * FROM kunde WHERE name LIKE @searchName;";
+
+                    try
+                    {
+                        string connectionString = GetConnectionString();
+
+                        using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+                        {
+                            con.Open();
+
+                            using (NpgsqlCommand cmd = new NpgsqlCommand(query, con))
+                            {
+                                cmd.Parameters.AddWithValue("@searchName", "%" + searchString + "%"); 
+                                using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                                {
+                                    DataTable table = new DataTable();
+                                    table.Load(reader);
+                                    dataGridViewKunde.DataSource = table;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Fehler beim Verbinden zur Datenbank: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string searchString = textBox2.Text.Trim();
+
+                if (!string.IsNullOrEmpty(searchString))
+                {
+
+                    string query = "SELECT * FROM artikel WHERE name LIKE @searchName;";
+
+                    try
+                    {
+                        string connectionString = GetConnectionString();
+
+                        using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+                        {
+                            con.Open();
+
+                            using (NpgsqlCommand cmd = new NpgsqlCommand(query, con))
+                            {
+                                cmd.Parameters.AddWithValue("@searchName", "%" + searchString + "%");
+                                using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                                {
+                                    DataTable table = new DataTable();
+                                    table.Load(reader);
+                                    dataGridViewArtikel.DataSource = table;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Fehler beim Verbinden zur Datenbank: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (int.TryParse(textBox3.Text, out int searchArtikelid))
+                {
+                    string query = "SELECT * FROM rechnung WHERE rechnungid = @searchRechnungid;";
+
+                    try
+                    {
+                        string connectionString = GetConnectionString();
+
+                        using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+                        {
+                            con.Open();
+
+                            using (NpgsqlCommand cmd = new NpgsqlCommand(query, con))
+                            {
+                                cmd.Parameters.AddWithValue("@searchRechnungid", searchArtikelid);
+                                using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                                {
+                                    DataTable table = new DataTable();
+                                    table.Load(reader);
+                                    dataGridViewRechnung.DataSource = table;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Fehler beim Verbinden zur Datenbank: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
